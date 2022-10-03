1dff49092173ed93d6d0832fcd58b8ec5be1714a
mario <manriqueduran123@gmail.com>
2022-10-03 17:02:47 -0500

    added style and the other sections of the mobile version

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@ import "./normalize.css";
 import kitchen from "./images/kitchen.jpg";
 
 const content = document.querySelector("#content");
-content.classList.add("content");
+const logoContainer = document.createElement("div");
+const bannerContainer = document.createElement("div");
+const chipContainer = document.createElement("div");
+const hoursContainer = document.createElement("div");
+const mykitchen = new Image();
+mykitchen.src = kitchen;
 
+content.classList.add("content");
+logoContainer.classList.add("logo-container");
+bannerContainer.classList.add("banner-container");
+chipContainer.classList.add("chip-container");
+hoursContainer.classList.add("hours-container");
+mykitchen.classList.add("kitchen-img")
+
+content.insertAdjacentElement("beforeend", logoContainer);
+content.insertAdjacentElement("beforeend", bannerContainer);
+content.insertAdjacentElement("beforeend", chipContainer);
+content.insertAdjacentElement("beforeend", hoursContainer);
+bannerContainer.appendChild(mykitchen);
